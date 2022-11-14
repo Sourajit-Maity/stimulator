ff49006eb8ac22f2fa5f6d93d057a41a906da921
SourajitMaity <sourajitm8@gmail.com>
2022-11-14 12:20:00 +0530

    saledropdown

--- app/Http/Livewire/Overallsale/OverallsaleCreateEdit.php
+++ app/Http/Livewire/Overallsale/OverallsaleCreateEdit.php
@@ -5,6 +5,9 @@ namespace App\Http\Livewire\Overallsale;
 use Livewire\Component;
 use App\Http\Livewire\Traits\AlertMessage;
 use App\Models\OverallSales;
+use App\Models\Rodtep;
+use App\Models\Air;
+use App\Models\WasteScrap;
 use Illuminate\Validation\Rule;
 use Livewire\WithFileUploads;
 use Spatie\MediaLibrary\Models\Media;
@@ -13,13 +16,12 @@ class OverallsaleCreateEdit extends Component
 {
     use WithFileUploads;
     use AlertMessage;
-    public $overallsale, $product_name,$description,$hsn,$export_value, $sez_unit, $dta, $deemed_export, 
-    $taxability_under_gst, $air,$sale_value_of_scrap, $waste_scrap,  $rodtep, $air_receivable,   $air_turnaround, $brand_rate, $air_amount,  $waste_scrap_amount, $rodtep_amount;
+    public $overallsale,$rodtepList, $wasteList,$aitList,  $product_name,$description,$hsn,$export_value, $sez_unit, $dta, $deemed_export, 
+    $taxability_under_gst,$blankArr, $air,$sale_value_of_scrap, $waste_scrap,  $rodtep, $air_receivable,   $air_turnaround, $brand_rate, $air_amount,  $waste_scrap_amount, $rodtep_amount;
     public $address;
     public $isEdit = false;
-    public $statusList = [];
-    public $industryList = [];
-    public $activityList = [];
+    public $taxabilityList = [];
+    public $airreceiveList = [];
     public $photo;
     public $photos = [];
     public $model_image, $imgId, $model_documents;
@@ -39,7 +41,22 @@ class OverallsaleCreateEdit extends Component
             ['value' => 1, 'text' => "Active"],
             ['value' => 0, 'text' => "Inactive"]
         ];
-      
+        $this->taxabilityList = [
+            ['value' => 0, 'text' => "Choose Taxability under GST"],
+            ['value' => 1, 'text' => "Taxable"],
+            ['value' => 0, 'text' => "Exempt"]
+        ];
+        $this->airreceiveList = [
+            ['value' => 0, 'text' => "Choose AIR receivable"],
+            ['value' => 1, 'text' => "Yes"],
+            ['value' => 0, 'text' => "No"]
+        ];
+        $this->blankArr = [
+            "value"=> "", "text"=> "== Select One =="
+        ];
+        $this->rodtepList = Rodtep::get();
+        $this->wasteList = WasteScrap::get();
+        $this->aitList = Air::get();
         
     }
 
@@ -55,16 +72,16 @@ class OverallsaleCreateEdit extends Component
                 'dta' => ['required'],
                 'deemed_export' => ['required'],
                 'taxability_under_gst' => ['required'],
-                'air' => ['nullable'],
-                'waste_scrap' => ['nullable'],
-                'brand_rate' => ['nullable'],
-                'rodtep' => ['nullable'],
-                'air_receivable' => ['nullable'],
-                'air_turnaround' => ['nullable'],
-                'air_amount' => ['nullable'],
-                'rodtep_amount' => ['nullable'],
-                'waste_scrap_amount' => ['nullable'],
-                'sale_value_of_scrap' => ['nullable'],
+                'air' => ['required'],
+                'waste_scrap' => ['required'],
+                'rodtep' => ['required'],
+                'air_receivable' => ['required'],
+                'air_turnaround' => ['required'],
+                'air_amount' => ['required'],
+                'rodtep_amount' => ['required'],
+                'waste_scrap_amount' => ['required'],
+                'sale_value_of_scrap' => ['required'],
+                'brand_rate' => ['required'],
 
 
             ];
@@ -81,24 +98,29 @@ class OverallsaleCreateEdit extends Component
                 'dta' => ['required'],
                 'deemed_export' => ['required'],
                 'taxability_under_gst' => ['required'],
-                'air' => ['nullable'],
-                'waste_scrap' => ['nullable'],
-                'rodtep' => ['nullable'],
-                'air_receivable' => ['nullable'],
-                'air_turnaround' => ['nullable'],
-                'air_amount' => ['nullable'],
-                'rodtep_amount' => ['nullable'],
-                'waste_scrap_amount' => ['nullable'],
-                'sale_value_of_scrap' => ['nullable'],
-                'brand_rate' => ['nullable'],
+                'air' => ['required'],
+                'waste_scrap' => ['required'],
+                'rodtep' => ['required'],
+                'air_receivable' => ['required'],
+                'air_turnaround' => ['required'],
+                'air_amount' => ['required'],
+                'rodtep_amount' => ['required'],
+                'waste_scrap_amount' => ['required'],
+                'sale_value_of_scrap' => ['required'],
+                'brand_rate' => ['required'],
             ];
     }
 
     public function saveOrUpdate()
     {
-        $this->air_amount = $this->export_value * $this->air;
-        $this->rodtep_amount = $this->deemed_export * $this->rodtep;
-        $this->waste_scrap_amount = $this->export_value * $this->waste_scrap;
+
+        $air_per = Air::where('id',(int)$this->air)->value('air_actual');  
+        $waste_per = WasteScrap::where('id',(int)$this->waste_scrap)->value('waste_scrap_actual');
+        $rodtep_per = Rodtep::where('id',(int)$this->rodtep)->value('rodtep_actual');
+
+        $this->air_amount = $this->export_value * $air_per;
+        $this->rodtep_amount = $this->deemed_export * $rodtep_per;
+        $this->waste_scrap_amount = $this->export_value * $waste_per;
         $this->sale_value_of_scrap = $this->rodtep_amount;
 
         $this->overallsale->fill($this->validate($this->isEdit ? $this->validationRuleForUpdate() : $this->validationRuleForSave()))->save();
--- resources/views/livewire/overallsale/overallsale-create-edit.blade.php
+++ resources/views/livewire/overallsale/overallsale-create-edit.blade.php
@@ -22,12 +22,12 @@
                     </x-admin.form-group>
                     <x-admin.form-group>
                         <x-admin.lable value="Sez unit" required />
-                        <x-admin.input type="text" wire:model.defer="sez_unit" placeholder="sez unit"  class="{{ $errors->has('sez_unit') ? 'is-invalid' :'' }}" />
+                        <x-admin.input type="text" wire:model.defer="sez_unit" placeholder="Sez unit"  class="{{ $errors->has('sez_unit') ? 'is-invalid' :'' }}" />
                         <x-admin.input-error for="sez_unit" />
                     </x-admin.form-group>
                     <x-admin.form-group>
                         <x-admin.lable value="Dta"  required />
-                        <x-admin.input type="text" wire:model.defer="dta" placeholder="dta"  class="{{ $errors->has('dta') ? 'is-invalid' :'' }}" />
+                        <x-admin.input type="text" wire:model.defer="dta" placeholder="Dta"  class="{{ $errors->has('dta') ? 'is-invalid' :'' }}" />
                         <x-admin.input-error for="dta" />
                     </x-admin.form-group>
                     <x-admin.form-group>
@@ -36,42 +36,70 @@
                         <x-admin.input-error for="deemed_export" />
                     </x-admin.form-group>
                     <x-admin.form-group>
-                        <x-admin.lable value="Taxability under Gst" required />
-                        <x-admin.input type="text" wire:model.defer="taxability_under_gst" placeholder="taxability under gst" autocomplete="off" class="{{ $errors->has('taxability_under_gst') ? 'is-invalid' :'' }}"/>
+                        <x-admin.lable value="Taxability under Gst" />
+                            <x-admin.dropdown  wire:model.defer="taxability_under_gst" placeHolderText="Please select one" autocomplete="off" class="{{ $errors->has('taxability_under_gst') ? 'is-invalid' :'' }}">
+                                    @foreach ($taxabilityList as $status)
+                                        <x-admin.dropdown-item  :value="$status['value']" :text="$status['text']"/>                          
+                                    @endforeach
+                            </x-admin.dropdown>
                         <x-admin.input-error for="taxability_under_gst" />
                     </x-admin.form-group>
-
+                    
 
                     <x-admin.form-group>
-                        <x-admin.lable value="Air" required />
-                        <x-admin.input type="text" wire:model.defer="air" placeholder="Air"  class="{{ $errors->has('air') ? 'is-invalid' :'' }}" />
-                        <x-admin.input-error for="air" />
-                    </x-admin.form-group>
-                    <x-admin.form-group>
-                        <x-admin.lable value="Waste scrap"  required />
-                        <x-admin.input type="text" wire:model.defer="waste_scrap" placeholder="waste scrap"  class="{{ $errors->has('waste_scrap') ? 'is-invalid' :'' }}" />
+                        <x-admin.lable value="Waste scrap" required/>
+                        <x-admin.dropdown  wire:model.defer="waste_scrap" placeHolderText="Please select Waste scrap" autocomplete="off" class="{{ $errors->has('waste_scrap') ? 'is-invalid' :'' }}">
+                            <x-admin.dropdown-item  :value="$blankArr['value']" :text="$blankArr['text']"/> 
+                                    @if(isset($wasteList))
+                                        @foreach($wasteList as $waste)
+                                        <x-admin.dropdown-item  :value="$waste->id" :text="$waste->waste_scrap_percentage"/>
+                                        @endforeach
+                                    @endif
+                            </x-admin.dropdown>
                         <x-admin.input-error for="waste_scrap" />
                     </x-admin.form-group>
                     <x-admin.form-group>
-                        <x-admin.lable value="Rodtep" required />
-                        <x-admin.input type="text" wire:model.defer="rodtep" placeholder="Rodtep" autocomplete="off" class="{{ $errors->has('rodtep') ? 'is-invalid' :'' }}"/>
+                        <x-admin.lable value="Air" required/>
+                        <x-admin.dropdown  wire:model.defer="air" placeHolderText="Please select state" autocomplete="off" class="{{ $errors->has('air') ? 'is-invalid' :'' }}">
+                            <x-admin.dropdown-item  :value="$blankArr['value']" :text="$blankArr['text']"/> 
+                                    @if(isset($aitList))
+                                        @foreach($aitList as $ait)
+                                        <x-admin.dropdown-item  :value="$ait->id" :text="$ait->air_percentage"/>
+                                        @endforeach
+                                    @endif
+                            </x-admin.dropdown>
+                            <x-admin.input-error for="air" />
+                    </x-admin.form-group>
+                    <x-admin.form-group>
+                        <x-admin.lable value="Rodtep" required/>
+                            <x-admin.dropdown  wire:model.defer="rodtep" placeHolderText="Please select Rodtep" autocomplete="off" class="{{ $errors->has('rodtep') ? 'is-invalid' :'' }}">
+                                <x-admin.dropdown-item  :value="$blankArr['value']" :text="$blankArr['text']"/> 
+                                    @if(isset($rodtepList))
+                                        @foreach($rodtepList as $rodt)
+                                        <x-admin.dropdown-item  :value="$rodt->id" :text="$rodt->rodtep_percentage"/>
+                                        @endforeach
+                                    @endif
+                            </x-admin.dropdown>
                         <x-admin.input-error for="rodtep" />
                     </x-admin.form-group>
                     <x-admin.form-group>
-                        <x-admin.lable value="Air receivable" required />
-                        <x-admin.input type="text" wire:model.defer="air_receivable" placeholder="air receivable" autocomplete="off" class="{{ $errors->has('air_receivable') ? 'is-invalid' :'' }}"/>
+                        <x-admin.lable value="AIR receivable" />
+                            <x-admin.dropdown  wire:model.defer="air_receivable" placeHolderText="Please select one" autocomplete="off" class="{{ $errors->has('air_receivable') ? 'is-invalid' :'' }}">
+                                    @foreach ($airreceiveList as $status)
+                                        <x-admin.dropdown-item  :value="$status['value']" :text="$status['text']"/>                          
+                                    @endforeach
+                            </x-admin.dropdown>
                         <x-admin.input-error for="air_receivable" />
                     </x-admin.form-group>
-
                     <x-admin.form-group>
                         <x-admin.lable value="Air turnaround" required />
-                        <x-admin.input type="text" wire:model.defer="air_turnaround" placeholder="air turnaround" autocomplete="off" class="{{ $errors->has('air_turnaround') ? 'is-invalid' :'' }}"/>
+                        <x-admin.input type="text" wire:model.defer="air_turnaround" placeholder="Air Turnaround" autocomplete="off" class="{{ $errors->has('air_turnaround') ? 'is-invalid' :'' }}"/>
                         <x-admin.input-error for="air_turnaround" />
                     </x-admin.form-group>
 
                     <x-admin.form-group>
-                        <x-admin.lable value="Brand_rate" required />
-                        <x-admin.input type="text" wire:model.defer="brand_rate" placeholder="brand rate" autocomplete="off" class="{{ $errors->has('brand_rate') ? 'is-invalid' :'' }}"/>
+                        <x-admin.lable value="Brand Rate" required />
+                        <x-admin.input type="text" wire:model.defer="brand_rate" placeholder="Brand Rate" autocomplete="off" class="{{ $errors->has('brand_rate') ? 'is-invalid' :'' }}"/>
                         <x-admin.input-error for="brand_rate" />
                     </x-admin.form-group>
                    
